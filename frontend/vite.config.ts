1e9e0c94eb6698bd30d01712e50af4d1e46b544a
TaiyoMaruyama <stickytomatobaby0501@gmail.com>
2024-12-05 23:00:10 +0900

    KumaUIライブラリのインストールと初期設定

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import KumaUI from "@kuma-ui/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), KumaUI({ wasm: true })],
+});
